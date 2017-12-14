cf6be334fb6844459bf679239366abf58db5e835
Kainy Guo <gt@kainy.cn>
2017-12-14 15:30:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 AV.init({
-  appId: 'ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333',
-  appKey: 'ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav',
+  appId: '8RLDamMl5A27EOhFH2fU7AN0-gzGzoHsz',
+  appKey: 'gVpxIyNY3brM8JXwCTGSNeG3',
 })
 
 var Todo = AV.Object.extend('Todo')
